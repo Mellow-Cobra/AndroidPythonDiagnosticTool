88ebea9994451f0dc10775706d95dcd39411fba4
Mellow-Cobra <internet.user.personal@gmail.com>
2024-08-03 16:33:47 -0700

    added test report generation and new headers for cpu diagnostics.

--- diagnostics/android_cpu_diagnostics.py
+++ diagnostics/android_cpu_diagnostics.py
@@ -1,11 +1,13 @@
 # Local Imports
 import csv
+import datetime
+import os
 from android_devices.adb_interface import AdbInterface
 from constants import *
 
-
 # Third Part Imports
 import pandas as pd
+
 
 class AndroidCpuDiagnostics:
     """Android CPU Diagnostics class"""
@@ -14,7 +16,6 @@ class AndroidCpuDiagnostics:
         """Constructor"""
         self.adb_interface = AdbInterface(serial_number)
         self.test_results = []
-
 
     def run_cpu_diagnostics(self):
         """Method used to run CPU diagnostics"""
@@ -28,50 +29,35 @@ class AndroidCpuDiagnostics:
         min_frequencies = self.adb_interface.get_cpu_min_speed()
         max_frequencies = self.adb_interface.get_cpu_max_speed()
         cpu_frequencies = self.adb_interface.get_cpu_frequencies()
+        self.test_results.append(CPU_FREQUENCY_DIAGNOSTIC_HEADER)
         for index, frequency in enumerate(cpu_frequencies):
-            if max_frequencies[index] >= frequency <= min_frequencies[index]:
-                cpu_freq_diag_test_status = {"CPU": index,
-                                             "Max_Frequency": max_frequencies[index],
-                                             "Frequency": frequency,
-                                             "Min_Frequency": min_frequencies[index],
-                                             "Units": MHZ
-                                             }
+            if float(max_frequencies[index]) >= frequency >= float(min_frequencies[index]):
+                cpu_freq_diag_test_status = [index, min_frequencies[index], frequency, max_frequencies[index],
+                                             MHZ, CPU_IN_SPEC, PASS]
                 self.test_results.append(cpu_freq_diag_test_status)
             else:
-                cpu_freq_diag_test_status  = {"CPU": index,
-                                              "Max_Frequency": max_frequencies[index],
-                                              "Frequency": frequency,
-                                              "Min_Frequency": min_frequencies[index],
-                                              "Units": MHZ
-                                              }
+                cpu_freq_diag_test_status = [index, min_frequencies[index], frequency, max_frequencies[index],
+                                             MHZ, CPU_FREQ_NOT_IN_SPEC, FAIL]
                 self.test_results.append(cpu_freq_diag_test_status)
-
 
     def evaluate_cpu_temperatures(self):
         """Method used to evaluate cpu temperatures"""
         cpu_temperatures = self.adb_interface.get_cpu_temperatures()
+        self.test_results.append(CPU_TEMPERATURE_DIAGNOSTIC_HEADER)
         for index, temperature in enumerate(cpu_temperatures):
-            if temperature < MAX_TEMPERATURE_LIMIT_C:
-                cpu_temperature_test_status = {"CPU": index,
-                                               "Max_Temperature": MAX_TEMPERATURE_LIMIT_C,
-                                               "Temperature Diagnostic Read out": temperature,
-                                               "Diagnostic_Result": CPU_TEMP_SAFE,
-                                               "Units": CELSIUS
-                                               }
+            if MIN_TEMPERATURE_LIMIT <= float(temperature) < MAX_TEMPERATURE_LIMIT_C:
+                cpu_temperature_test_status = [index, MAX_TEMPERATURE_LIMIT_C, temperature, MIN_TEMPERATURE_LIMIT,
+                                               CELSIUS, CPU_NO_THROTTLE , PASS]
                 self.test_results.append(cpu_temperature_test_status)
             else:
-                cpu_temperature_test_status = {"CPU": index,
-                                               "Max_Temperature": MAX_TEMPERATURE_LIMIT_C,
-                                               "Temperature Diagnostic Read out": temperature,
-                                               "Diagnostic_Result": CPU_THROTTLE_OT,
-                                               "Units": CELSIUS}
+                cpu_temperature_test_status = [index, MAX_TEMPERATURE_LIMIT_C, temperature, MIN_TEMPERATURE_LIMIT,
+                                               CELSIUS, CPU_THROTTLE_OT, FAIL]
                 self.test_results.append(cpu_temperature_test_status)
-
-
 
     def generate_cpu_diagnostic_test_report(self):
         """Method used to generate test report for CPU diagnostics"""
-        cpu_df = pd.DataFrame(self.test_results)
-        print(cpu_df)
-
-
+        time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+        os.chdir('C:\\Users\\inter\\OneDrive\\Documents\\Results')
+        with open(f'{time_stamp}.csv', mode='w', newline='') as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerows(self.test_results)
