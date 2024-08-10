3a90aa3082b1d91c8883b568a7e2b06af770ddea
Mellow-Cobra <internet.user.personal@gmail.com>
2024-08-10 09:22:09 -0700

    added configuration file. added configuration file path field and load button redid configuration tab layout added more sublayouts to organize configuration tab layout.

--- diagnostics/android_wifi_diagnostics.py
+++ diagnostics/android_wifi_diagnostics.py
@@ -1,4 +1,5 @@
 # Standard Imports
+import logging
 import subprocess
 import os
 
@@ -6,6 +7,7 @@ import os
 # Local Imports
 from android_devices.adb_interface import AdbInterface
 
+logger = logging.getLogger(__name__)
 
 class WifiDiagnostics():
     """Class used for Android device wifi diagnostics"""
@@ -15,7 +17,8 @@ class WifiDiagnostics():
         self.adb_interface = AdbInterface(serial_number)
 
     def wifi_level_one_diagnostics(self):
-        self.adb_interface.get_wifi_status()
+        wifi_status = self.adb_interface.get_wifi_status()
+
         self.adb_interface.get_wifi_network_info()
         self.adb_interface.get_wifi_internet_status()
         self.adb_interface.run_ping_test()
@@ -26,6 +29,9 @@ class WifiDiagnostics():
         """Method used to verify wifi radio functionality"""
         self.adb_interface.set_global_wifi_off()
         self.adb_interface.set_global_wifi_on()
+
+    def generate_wifi_test_report(self):
+        """Method used to generate WiFi test report"""
 
 if __name__ == '''__main__''':
     w = WifiDiagnostics(serial_number=375010008142000055)
